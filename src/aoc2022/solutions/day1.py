a2fa0940b5a9921acc52d1270ddf57ac82bf2e0a
Facundo Martínez <facundo.martinez@lingokids.com>
2022-12-02 00:30:55 +0100

    Code for day 1

---
+++ src/aoc2022/solutions/day1.py
@@ -0,0 +1,15 @@
+from typing import TextIO
+
+import click
+
+from aoc2022.solutions import Elf, elves_in_file
+
+
+@click.command()
+@click.argument("input_file", type=click.File())
+def command(input_file: TextIO):
+    most_caloric_elf: Elf = None
+    for elf in elves_in_file(input_file):
+        if not most_caloric_elf or (most_caloric_elf and most_caloric_elf.calories < elf.calories):
+            most_caloric_elf = elf
+    print(most_caloric_elf)
